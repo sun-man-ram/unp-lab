9471dd00686933e86fb82887ad76f527e62f33d2
sun-man-ram <manikantasunkara2653@gmail.com>
2024-07-27 18:32:08 +0530

    count commans error

--- main.c
+++ main.c
@@ -1,89 +1,90 @@
-#include<stdio.h>
+#include <stdio.h>
 #include <stdlib.h>
-#include<string.h>
+#include <string.h>
 
-int main(int argc,char *argv[]){
-    char *input_filename=argv[1];
-    printf("%s\n",input_filename);
-    FILE *file = fopen(input_filename, "r");
-   char line[256];
-   int commas=0;
-   while (fgets(line, sizeof(line), file)) {
-   line[strcspn(line, "\n")] = 0;
-   printf("%s\n",line);
+int main(int argc, char *argv[])
+{
+  char *input_filename = argv[1];
+  printf("%s\n", input_filename);
+  FILE *file = fopen(input_filename, "r");
+  char line[256];
+  int commas = 0;
+  while (fgets(line, sizeof(line), file))
+  {
+    line[strcspn(line, "\n")] = 0;
+    printf("%s\n", line);
 
-    if(strcmp(line,"# add students/n")==0){
-
+    if (strcmp(line, "# add students/n") == 0)
+    {
     }
-    else if(strcmp(line,"# initial addition of database")==0){
-        fgets(line, sizeof(line), file);
-        while(1){
-     //check no of commas 
-     //accordingly select the student name row or course and mark row
-     
-     for(int i=0;i<sizeof(line)/sizeof(char);i++){
-        if(line[i]==','){
-            commas++;
-        }
-     }
-     
-      if(commas>1){
-        // stduent name data base row
-        // 1) i need to add this guy in linked list and 
-        //check whether he is nique or not 
-        // create a course linked list for his name under roll no
-        printf("hello chintu this is name");
-         
-       
-       
-         
-        
-         
-
-
-
-
-
-
-
-
-      }
-      else{
-        //course code and marks 
-        // add the course in the linekd list and check whether it is unique or not
-      printf("hello mani this course part");
-      }
+    else if (strcmp(line, "# initial addition of database") == 0)
+    {
       fgets(line, sizeof(line), file);
-      if(line[0] == '#')
+      while (1)
       {
-        break;
+        commas=0;
+        // check no of commas
+        // accordingly select the student name row or course and mark row
+
+        for (int i = 0; i < sizeof(line) / sizeof(char); i++)
+        {
+          if (line[i] == ',')
+          {
+            commas++;
+           
+          }
+        }
+         printf("%d\n",commas);
+        if (commas > 1)
+        {
+
+          // printf("%s",line);
+          // stduent name data base row
+          // 1) i need to add this guy in linked list and
+          // check whether he is nique or not
+          // create a course linked list for his name under roll no
+          printf("hello chintu this is name\n");
+          commas = 0;
+        }
+        else if(commas == 1)
+        {
+          // course code and marks
+          //  add the course in the linekd list and check whether it is unique or not
+          printf("hello mani this course part");
+          commas = 0;
+        }
+        
+        fgets(line, sizeof(line), file);
+        line[strcspn(line, "\n")] = 0;
+
+        if (line[0] == '#')
+        {
+          break;
+        }
       }
+    }
+    else if (strcmp(line, "# add course"))
+    {
+      // printf("%s","Add the course");
+      //  printf("tony");
+    }
+    else if (strcmp(line, "# modify student"))
+    {
+      // printf("%s","Add the course");
+    }
+    else if (strcmp(line, "# modify course"))
+    {
+    }
+    else if (strcmp(line, "# delete student"))
+    {
+    }
+    else if (strcmp(line, "# delete course"))
+    {
+    }
   }
 
-    }
-    else if(strcmp(line,"# add course")){
-        //printf("%s","Add the course");
-    }
-    else if(strcmp(line,"# modify student")){
-        //printf("%s","Add the course");
-    }
-    else if(strcmp(line,"# modify course")){
+  // we are reading the file that mam gave
+  // process the data
 
-    }
-    else if(strcmp(line,"# delete student")){
-
-    }
-    else if(strcmp(line,"# delete course")){
-
-    }
-    }
-     
-
-  // we are reading the file that mam gave 
-  // process the data 
-    
-   
-
-
-    return 0;
+  return 0;
 }
